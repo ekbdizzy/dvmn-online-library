dc8a4d7857d2652a3866939a26f9d0faaba8d0ed
Aleksey Koshkin <ekbdizzy@yandex.ru>
2021-09-16 19:42:25 +0300

    Making paginator

--- render_website.py
+++ render_website.py
@@ -1,4 +1,6 @@
 import json
+import math
+
 from more_itertools import chunked
 from pathlib import Path
 from livereload import Server
@@ -11,6 +13,15 @@ PAGES_FOLDER = 'pages'
 DEFAULT_PAGE_NAME = 'index'
 BOOKS_QUANTITY_ON_PAGE = 10
 BASE_URL = '/'
+
+
+def get_pages_info(books: list) -> list:
+    pages = []
+    for page in range(1, math.ceil(len(books) / BOOKS_QUANTITY_ON_PAGE)):
+        pages.append({"number": page,
+                      "link": urljoin(BASE_URL, f'{PAGES_FOLDER}/{DEFAULT_PAGE_NAME}{page}.html')
+                      })
+    return pages
 
 
 def update_urls_in_books(books: list, base_url: str = BASE_URL):
@@ -32,17 +43,21 @@ def save_page(rendered_page: str,
 def split_books_by_pages(books: list,
                          folder: str = PAGES_FOLDER,
                          file_name: str = DEFAULT_PAGE_NAME,
-                         books_quantity_on_page: int = 10) -> None:
+                         books_quantity_on_page: int = BOOKS_QUANTITY_ON_PAGE) -> None:
     """Read books and save split html-pages by paths ./folder/index{page_index}.html."""
     for page_index, books_chunk in enumerate(chunked(books, books_quantity_on_page), 1):
         template = env.get_template(TEMPLATE_HTML)
-        rendered_page = template.render(books=update_urls_in_books(books_chunk))
+        rendered_page = template.render(books=update_urls_in_books(books_chunk),
+                                        pages=get_pages_info(books))
         save_page(rendered_page, page_index, folder, file_name)
 
 
 def on_reload():
     template = env.get_template(TEMPLATE_HTML)
-    rendered_page = template.render(books=update_urls_in_books(books))
+    rendered_page = template.render(
+        books=update_urls_in_books(books),
+        pages=get_pages_info(books)
+    )
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
     split_books_by_pages(books)
